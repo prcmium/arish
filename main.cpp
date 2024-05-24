78ed45e7a74c3053f235d43ca9548d35966641f4
prcmium <chrayl.97@gmail.com>
2024-05-24 22:56:16 +0300

    Dinic's

--- main.cpp
+++ main.cpp
@@ -1,76 +1,79 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-struct Edge{
-    int v; // в какую верщину ведет
-    int flow; 
-    int capacity; 
+const int INF = 1e9; 
+int n, m; // n - количество вершин, m - количество рёбер
+vector<vector<int>> c_flow; 
+vector<vector<int>> adj; 
 
-    Edge(int v, int capacity)
-        : v(v), flow(0), capacity(capacity) {}
-
-    int get_capacity() { 
-        return capacity - flow;
+bool bfs(vector<int>& d, int s, int t) { // строим вспомогательную сеть
+    fill(d.begin(), d.end(), INF);
+    d[s] = 0;
+    queue<int> A;
+    A.push(s);
+    while (!A.empty()) {
+        int u = A.front();
+        A.pop();
+        for (int v : adj[u]) {
+            if (c_flow[u][v] > 0 && d[v] == INF) {
+                d[v] = d[u] + 1;
+                A.push(v);
+            }
+        }
     }
-};
-
-const int INF = (int)(1e9) + 666;
-const int N = 666;
-int S, T; 
-
-vector<Edge> edges;
-vector<int> graph[N]; 
-int used[N]; // массив использованных вершин
-int timer = 1; 
-
-void add_edge(int v, int u, int capacity) {
-    graph[v].emplace_back(edges.size()); 
-    edges.emplace_back(u, capacity); 
-    graph[u].emplace_back(edges.size()); 
-    edges.emplace_back(v, 0); 
+    return d[t] != INF;
 }
-int dfs(int v, int min_capacity) {
-    if (v == T) {
-       
-        return min_capacity;
+
+int dfs(int u, int minC, vector<int>& d, vector<int>& p, int t) { // ищем блокирующий поток
+    if (u == t || minC == 0) {
+        return minC;
     }
-    used[v] = timer;
-    for (int index : graph[v]) {
-        if (edges[index].get_capacity() == 0) {
-            continue; 
-        }
-        if (used[edges[index].v] == timer) {
-            continue;
-        }
-        int x = dfs(edges[index].v, min(min_capacity, edges[index].get_capacity()));
-        if (x) { 
-            edges[index].flow += x;
-            edges[index ^ 1].flow -= x;
-            return x;
+    for (int& i = p[u]; i < adj[u].size(); ++i) { // перебираем все вершины смежные u, начиная с последней проверенной
+        int v = adj[u][i];
+        if (d[v] == d[u] + 1) {
+            int delta = dfs(v, min(minC, c_flow[u][v]), d, p, t);
+            if (delta != 0) {
+                c_flow[u][v] -= delta;
+                c_flow[v][u] += delta;
+                return delta;
+            }
         }
     }
-  
     return 0;
+}
+
+int MaxFlow(int s, int t) {
+    int maxFlow = 0;
+    vector<int> d(n);
+    while (bfs(d, s, t)) { 
+        vector<int> p(n, 0);
+        int flow;
+        do {
+            flow = dfs(s, INF, d, p, t);
+            maxFlow += flow;
+        } while (flow != 0);
+    }
+    return maxFlow;
 }
 
 int main() {
-    int n, m;
-    cin >> n >> m >> S >> T;
+    cin >> n >> m; 
+    c_flow.assign(n, vector<int>(n, 0));
+    adj.assign(n, vector<int>());
+
+    int u, v, c;
     for (int i = 0; i < m; ++i) {
-        int v, u, capacity;
-        cin >> v >> u >> capacity; // v - откуда, u - куда, capacity - пропускная способность
-        add_edge(v, u, capacity);
+        cin >> u >> v >> c; 
+        adj[u].push_back(v); 
+        adj[v].push_back(u); 
+        c_flow[u][v] = c; 
     }
-    while (dfs(S, INF)) {  
-        ++timer;
-    }
-  
-    int result = 0;
-    for (int index : graph[S]) {
-        result += edges[index].flow;
-    }
-    cout << "Максимальный поток: " << result << endl;
-    return 0;
+
+    int s, t;
+    cin >> s >> t; 
+    cout << "Максимальный поток: " << MaxFlow(s, t) << '\n';
+    
 }
